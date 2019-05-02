e8ce8256f6364c0908c7d921c0f22c785444e95c
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-05-02 16:22:33 -0700

    Enable deployment on git push and add iTunes lib (#6)

---
+++ server/src/lib/itunes.ts
@@ -0,0 +1,41 @@
+import * as puppeteer from "puppeteer";
+
+export async function addItunesPodcast(rssFeedUrl) {
+  (async() => {
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+
+    const page = await browser.newPage();
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36");
+    await page.goto("https://itunesconnect.apple.com/login");
+    await page.waitFor(5000);
+    const frames = await page.frames();
+    const authFrame = frames.find(f => f.name() === "aid-auth-widget-iFrame");
+    const usernameField = await authFrame.$("#account_name_text_field");
+    await usernameField.type(process.env.ITUNES_USER);
+    let signInButton = await authFrame.$("#sign-in");
+    signInButton.click();
+    await page.waitFor(2000);
+
+    const passwordField = await authFrame.$("#password_text_field");
+    await passwordField.type(process.env.ITUNES_PASS);
+    signInButton = await authFrame.$("#sign-in");
+    signInButton.click();
+    await page.waitFor(2000);
+
+    await page.goto("https://podcastsconnect.apple.com/my-podcasts/new-feed");
+    const newFeedUrlField = await page.waitFor("#new-feed-url");
+    await newFeedUrlField.type(rssFeedUrl);
+    const validateFeedButton = await page.waitFor("button.tb-btn--primary.new-feed-validate");
+    validateFeedButton.click();
+
+    const submitFeedButton = await page.waitFor("button.tb-btn--primary.new-feed-submit");
+    await page.waitFor(10000);
+    submitFeedButton.click();
+    await page.waitFor(25000);
+    await page.screenshot({path: "submitted.png"});
+    await browser.close();
+  })();
+}
